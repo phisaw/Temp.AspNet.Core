3923e074810809150234f069adf504217152e150
phisaw <57394869+phisaw@users.noreply.github.com>
2020-10-15 14:28:14 +0200

    first commit

---
+++ UnitTestTemperature/UnitTestTemperature.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTemperature
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
